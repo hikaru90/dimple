7f1393dbe74cfc2c0cab9ad6beea41fb72c371b3
hikaru90 <alexbueckner@gmail.com>
2024-03-14 00:56:56 +0100

    add socials

--- components/Menu.vue
+++ components/Menu.vue
@@ -6,28 +6,36 @@
     expand: "subpages",
   });
 
+  const touchPoints = await pb.collection("contactOptions").getFullList(200, {
+    sort: "-order",
+  });
+
   const state = reactive({
     menuIsOpen: false,
     hoverIndex: null,
     menuEntryCount: 0,
+    currentTime: new Date(),
+    timeInterval: undefined,
   });
 
   const menuEntries = computed(() => {
-    let entries = records.filter(record => record.inMenu === true).sort((a,b) => a.sortOrder - b.sortOrder)
-    let i = 0
-    for(const entry of entries){
-      entry.delay = i * 100
-      i++
-      if(entry.subpages.length > 0){
-        for(const subpage of entry.expand.subpages){
-          subpage.delay = i * 100
-          i++
+    let entries = records
+      .filter((record) => record.inMenu === true)
+      .sort((a, b) => a.sortOrder - b.sortOrder);
+    let i = 0;
+    for (const entry of entries) {
+      entry.delay = i * 100;
+      i++;
+      if (entry.subpages.length > 0) {
+        for (const subpage of entry.expand.subpages) {
+          subpage.delay = i * 100;
+          i++;
         }
       }
     }
-    state.menuEntryCount = i
-    return entries
-  })
+    state.menuEntryCount = i;
+    return entries;
+  });
 
   const toggleMenu = () => {
     console.log("toggle");
@@ -39,6 +47,38 @@
     if (!slug) return;
     state.menuIsOpen = false;
   };
+
+  // const recalculateCurrentTime = () => {
+  //   state.currentTime = new Date();
+  // };
+
+  const isInOpeningHours = (link) => {
+    if (!link?.start && !link?.end) return true;
+
+    const startDay = new Date(link.start).getDay();
+    const endDay = new Date(link.end).getDay();
+    const currentWeekday = state.currentTime.getDay();
+    // Check if current day matches the weekday of start and end dates
+    if (currentWeekday < startDay || currentWeekday > endDay) {
+      return false;
+    }
+
+    const gmtOffset = 1;
+    const start = new Date(link.start).getHours() - gmtOffset;
+    const end = new Date(link.end).getHours() - gmtOffset;
+
+    return state.currentTime.getHours() >= start && state.currentTime.getHours() <= end;
+  };
+
+  // onMounted(() => {
+  //   state.timeInterval = setInterval(() => {
+  //     recalculateCurrentTime();
+  //   }, 2000);
+  // });
+
+  // onBeforeUnmount(() => {
+  //   clearInterval(state.timeInterval)
+  // });
 </script>
 
 <template>
@@ -57,7 +97,11 @@
             <NuxtLink to="/"> Dimple Goertz </NuxtLink>
           </div>
 
-          <button aria-label="Menü öffnen" @click="toggleMenu" class="flex items-center justify-center rounded p-1">
+          <button
+            aria-label="Menü öffnen"
+            @click="toggleMenu"
+            class="flex items-center justify-center rounded p-1"
+          >
             <nuxt-icon name="icon-cross" class="text-3xl text-red" />
           </button>
         </div>
@@ -75,33 +119,37 @@
                 :to="menuEntry.slug === 'index' ? '/' : menuEntry.slug"
                 :key="'menuEntry' + index"
                 :style="[{ transitionDelay: `${menuEntry.delay}ms` }]"
-                :class="[{ hidden: !menuEntry.slug }, state.menuIsOpen ? 'transition-all opacity-100' : 'opacity-0']"
+                :class="[
+                  { hidden: !menuEntry.slug },
+                  state.menuIsOpen ? 'transition-all opacity-100' : 'opacity-0',
+                ]"
                 class="select-none flex items-center transition-all px-3 py-1 rounded duration-300"
               >
                 <span>
                   {{ menuEntry.title }}
                 </span>
               </NuxtLink>
-                <!-- <nuxt-icon name="icon-triangle_up" class="block text-2xl -mb-3 text-white" /> -->
-                <div class="flex flex-col gap-3">
-                  <NuxtLink
-                    @click="closeMenu(menuEntry)"
-                    v-for="(subpage, i) in menuEntry.expand.subpages"
-                    :to="subpage.slug === 'index' ? '/' : subpage.slug"
-                    :key="index + 'subpage' + i"
-                    :style="[{ transitionDelay: `${subpage.delay}ms` }]"
-                    :class="[state.menuIsOpen ? 'transition-all opacity-100' : 'opacity-0']"
-                    class="whitespace-nowrap last:border-b-0 px-3 py-1 hover:bg-white hover:bg-opacity-5 rounded transition-all"
-                  >
-                    <!-- <span class="opacity-20">
+              <!-- <nuxt-icon name="icon-triangle_up" class="block text-2xl -mb-3 text-white" /> -->
+              <div class="flex flex-col gap-3">
+                <NuxtLink
+                  @click="closeMenu(menuEntry)"
+                  v-for="(subpage, i) in menuEntry.expand.subpages"
+                  :to="subpage.slug === 'index' ? '/' : subpage.slug"
+                  :key="index + 'subpage' + i"
+                  :style="[{ transitionDelay: `${subpage.delay}ms` }]"
+                  :class="[state.menuIsOpen ? 'transition-all opacity-100' : 'opacity-0']"
+                  class="whitespace-nowrap last:border-b-0 px-3 py-1 hover:bg-white hover:bg-opacity-5 rounded transition-all"
+                >
+                  <!-- <span class="opacity-20">
                     — 
                   </span> -->
-                    {{ subpage.title }}
-                  </NuxtLink>
+                  {{ subpage.title }}
+                </NuxtLink>
               </div>
             </div>
           </div>
-          <NuxtLink @click="closeMenu('buchen')"
+          <NuxtLink
+            @click="closeMenu('buchen')"
             to="/buchen"
             :style="[{ transitionDelay: `${(state.menuEntryCount + 1) * 100}ms` }]"
             :class="[state.menuIsOpen ? 'transition-all opacity-100' : 'opacity-0']"
@@ -114,6 +162,18 @@
               <nuxt-icon name="icon-caret-right" class="text-sm text-black" />
             </div>
           </NuxtLink>
+          <div v-if="touchPoints?.length > 0" class="flex items-center gap-2">
+            <a
+              v-for="(link, index) in touchPoints"
+              :href="link.link"
+              target="_blank"
+              :key="'link' + index"
+              :class="[isInOpeningHours(link) ? 'block' : 'hidden']"
+              class="flex items-center justify-center w-8 h-8 rounded-lg"
+            >
+              <nuxt-icon :name="link.icon" class="text-2xl text-black" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
@@ -121,18 +181,34 @@
     <div class="absolute top-0 left-0 w-full shadow z-20 text-white">
       <div class="max-container w-full">
         <div class="flex items-center justify-between py-6">
-          <div class="font-heading text-2xl">
+          <div class="font-heading text-3xl">
             <NuxtLink to="/"> Dimple Goertz </NuxtLink>
           </div>
 
           <div class="lg:hidden">
-            <button aria-label="Menü öffnen" @click="toggleMenu" class="flex items-center justify-center rounded p-1">
+            <button
+              aria-label="Menü öffnen"
+              @click="toggleMenu"
+              class="flex items-center justify-center rounded p-1"
+            >
               <nuxt-icon name="icon-menu" class="text-2xl text-white" />
             </button>
           </div>
           <div class="hidden lg:block">
             <div class="flex items-center gap-4">
-              <div class="flex items-center gap-4">
+              <div v-if="touchPoints?.length > 0" class="flex items-center gap-1">
+                <a
+                  v-for="(link, index) in touchPoints"
+                  :href="link.link"
+                  target="_blank"
+                  :key="'link' + index"
+                  :class="[isInOpeningHours(link) ? 'block' : 'hidden']"
+                  class="flex items-center justify-center w-6 h-6 rounded group"
+                >
+                  <nuxt-icon :name="link.icon" class="text-2xl text-white group-hover:text-gold transition" />
+                </a>
+              </div>
+              <div class="flex items-center gap-6 mx-3">
                 <div
                   v-for="(menuEntry, index) in menuEntries"
                   v-show="menuEntry.inMenu"
@@ -143,7 +219,7 @@
                     :to="menuEntry.slug === 'index' ? '/' : menuEntry.slug"
                     @mouseenter="state.hoverIndex = index"
                     :key="'menuEntry' + index"
-                    class="flex select-none items-center transition-all px-3 py-1 rounded"
+                    class="flex select-none items-center transition-all px-1 py-1 rounded"
                   >
                     <div>
                       {{ menuEntry.title }}
@@ -176,6 +252,7 @@
                   </div>
                 </div>
               </div>
+
               <NuxtLink
                 to="/buchen"
                 class="border-2 border-gold rounded flex items-center px-3 py-2"
